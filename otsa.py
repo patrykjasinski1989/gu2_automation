cea34d01ab2ebe9f8b8e78f896cafb27e8ede26b
zxnak37 <zxnak37@gmail.com>
2018-01-26 12:36:13 +0100

    Added support for transactions with no contracts

--- otsa.py
+++ otsa.py
@@ -15,6 +15,11 @@ def searchMsisdn(con, msisdn):
                 'crm_customer_id, t.custcode from ptk_otsa_transaction t, ptk_otsa_trans_contract con '
                 'where t.trans_code = con.trans_code (+) and con.msisdn = \'' + str(msisdn) + '\'')
     rows = cur.fetchall()
+    if len(rows) == 0:
+        cur.execute('select t.cart_code, t.trans_code, t.msisdn, t.status, t.ncs_trans_num, t.om_order_id, '
+                    't.process_error, t.ncs_error_desc, t.trans_type, t.author_user_code, t.trans_num, t.create_date, '
+                    'crm_customer_id, t.custcode from ptk_otsa_transaction t where t.msisdn = \'' + str(msisdn) + '\'')
+        rows = cur.fetchall()
     cur.close()
     dict_rows = []
     for row in rows:
@@ -30,8 +35,9 @@ def searchCart(con, cart_code):
     cur = con.cursor()
     try:
         cur.execute('select t.cart_code, t.trans_code, t.status, t.ncs_trans_num, t.om_order_id, '
-                't.process_error, t.ncs_error_desc, t.trans_type, t.author_user_code, t.trans_num, t.create_date, '
-                'crm_customer_id, t.custcode from ptk_otsa_transaction t where t.cart_code = \'' + str(cart_code) +'\'')
+                    't.process_error, t.ncs_error_desc, t.trans_type, t.author_user_code, t.trans_num, t.create_date, '
+                    'crm_customer_id, t.custcode from ptk_otsa_transaction t where t.cart_code = \'' + str(
+            cart_code) + '\'')
         rows = cur.fetchall()
     except:
         cur.close()
@@ -43,14 +49,15 @@ def searchCart(con, cart_code):
                           'ncs_trans_num': row[3], 'om_order_id': row[4], 'process_error': row[5],
                           'ncs_error_desc': row[6], 'trans_type': row[7], 'author_user_code': row[8],
                           'trans_num': row[9], 'create_date': row[10], 'crm_customer_id': row[11],
-			  'custcode': row[12]})
+                          'custcode': row[12]})
     return dict_rows
 
 
 def updateTransaction(con, trans_code, status):
     cur = con.cursor()
     cur.execute('update ptk_otsa_transaction set status = \'' + str(status) + '\' '
-                'where trans_code = \'' + str(trans_code) + '\'')
+                                                                              'where trans_code = \'' + str(
+        trans_code) + '\'')
     if cur.rowcount == 1:
         con.commit()
     else:
@@ -62,7 +69,8 @@ def updateTransaction(con, trans_code, status):
 def updateContract(con, trans_code, status):
     cur = con.cursor()
     cur.execute('update ptk_otsa_trans_contract set status = \'' + str(status) + '\' '
-                'where trans_code = \'' + str(trans_code) + '\'')
+                                                                                 'where trans_code = \'' + str(
+        trans_code) + '\'')
     if cur.rowcount == 1:
         con.commit()
     else:
@@ -110,8 +118,9 @@ def fixPesel(con, trans_code):
 
 def fixAAC(con, trans_code, bscs_customer_id):
     cur = con.cursor()
-    cur.execute("update ptk_otsa_transaction set bscs_customer_id = '" + str(bscs_customer_id) +"', new_customer = 'N'"
-                "where trans_code = '" + str(trans_code) + "'")
+    cur.execute("update ptk_otsa_transaction set bscs_customer_id = '" + str(bscs_customer_id) + "', new_customer = 'N'"
+                                                                                                 "where trans_code = '" + str(
+        trans_code) + "'")
     if cur.rowcount == 1:
         con.commit()
     else:
@@ -124,10 +133,11 @@ def checkSIM(con, sim):
     cur = con.cursor()
     cur.execute('select t.imei, tr.cart_code, t.trans_code, \'contract\', tr.create_date, tr.status, tr.trans_num '
                 'from ptk_otsa_transaction tr, ptk_otsa_trans_contract t where sm_serialnum = \'' + str(sim) + '\' '
-                'and t.trans_code=tr.trans_code union '
-                'select t.res, tr.cart_code, t.trans_code, \'reserved\', tr.create_date, tr.status, tr.trans_num '
-                'from ptk_otsa_transaction tr, ptk_otsa_trans_reserved_res t where t.res = \'' + str(sim) + '\' ' 
-                'and t.trans_code=tr.trans_code')
+                                                                                                               'and t.trans_code=tr.trans_code union '
+                                                                                                               'select t.res, tr.cart_code, t.trans_code, \'reserved\', tr.create_date, tr.status, tr.trans_num '
+                                                                                                               'from ptk_otsa_transaction tr, ptk_otsa_trans_reserved_res t where t.res = \'' + str(
+        sim) + '\' '
+               'and t.trans_code=tr.trans_code')
     rows = cur.fetchall()
     cur.close()
     dict_rows = []
@@ -140,7 +150,7 @@ def checkSIM(con, sim):
 def updateCart(con, trans_code, cart_code):
     cur = con.cursor()
     cur.execute('update ptk_otsa_transaction set cart_code = \'' + cart_code + '\' '
-                'where trans_code = \'' + trans_code + '\'')
+                                                                               'where trans_code = \'' + trans_code + '\'')
     if cur.rowcount == 1:
         con.commit()
     else:
