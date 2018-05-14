8ababd6fa7c5b37753ddef96f5c577bf28a73546
zxnak37 <zxnak37@gmail.com>
2018-05-14 20:35:16 +0200

    Added getting orders from ML db

--- ml.py
+++ ml.py
@@ -1,7 +1,19 @@
 # -*- coding: utf-8 -*-
 import cx_Oracle
+
 import config
 
 
 def ml_connection():
     return cx_Oracle.connect(user=config.ml['user'], password=config.ml['password'], dsn=config.ml['server'])
+
+
+def get_order_data(con, channel_order_id):
+    cur = con.cursor()
+    stmt = "select k.msisdn, k.idzleceniaom, z.id, z.status, z.datazmianystatusu " \
+           "from ml.kontrakty k join ml.zlecenia z on z.id = k.idzlecenia " \
+           "where k.idzleceniawkanale = '{}'".format(channel_order_id)
+    cur.execute(stmt)
+    row = cur.fetchone()
+    cur.close()
+    return {'msisdn': row[0], 'om_order_id': row[1], 'ml_id': row[2], 'status': row[3], 'last_status_change': row[4]}
