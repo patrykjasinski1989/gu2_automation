20580854be9ecc0e34fd28be13263f958f358f27
zxnak37 <zxnak37@gmail.com>
2018-09-05 16:22:36 +0200

    Added sample config file

---
+++ config_sample.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+remedy = {'server': '126.185.108.140',
+          'port': 60000,
+          'user': '',
+          'password': ''}
+
+muchomor = {'proxies': dict(http='socks5://172.30.70.158:6655', https='socks5://172.30.70.158:6655'),
+            'url': 'http://126.16.23.48:8082/Muchomor-App/',
+            'user': '',
+            'password': ''}
+
+om = {'server': 'http://172.30.70.10:6655',
+      'user': '',
+      'password': ''}
+
+otsa = {'server': '10.236.28.107:1521/OTSA',
+        'user': '',
+        'password': ''}
+
+rsw = {'server': '10.236.28.53:1521/RSW',
+       'user': '',
+       'password': ''}
+
+nra = {'server': '10.236.28.66:1526/NRA',
+       'user': '',
+       'password': ''}
+
+bscs = {'server': '10.236.28.81:1526/BSCS',
+        'user': '',
+        'password': ''}
+
+opti = {'server': '10.236.28.114:1551/OPTIPOS',
+        'user': '',
+        'password': ''}
+
+eai = {'server': 'http://10.236.14.11:4125'}
+
+ml = rsw
+
+optpos_logs = {'server': '172.30.56.129',
+               'user': '',
+               'password': ''}
