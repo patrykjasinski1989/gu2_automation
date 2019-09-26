aed6a2af267b050dfac3b5665d6ecdf605310957
zxnak37 <zxnak37@gmail.com>
2019-09-26 09:57:56 +0200

    Added getting and displaying ord_id for orders to be processed manually

--- gu2_int_robot.py
+++ gu2_int_robot.py
@@ -2,6 +2,7 @@
 import sys
 
 from helper_functions import has_brm_error, get_tel_order_number, get_logs_for_order
+from om_tp import get_order_data, get_order_info
 from remedy import get_all_incidents, get_work_info, has_exactly_one_entry, add_work_info, reassign_incident
 
 
@@ -18,7 +19,9 @@ def handle_brm_errors():
                     add_work_info(inc, 'VC_OM', logs_string)
                     reassign_incident(inc, 'APLIKACJE_OBRM_DOSTAWCA')
                 else:
-                    print('{} {} {}'.format(inc['inc'], tel_order_number, logs_string), file=sys.stderr)
+                    order_data = get_order_data(get_order_info(tel_order_number))
+                    ord_id = order_data['OM zamowienie (ORD_ID)']
+                    print('{} {} {} {}'.format(inc['inc'], ord_id, tel_order_number, logs_string), file=sys.stderr)
 
 
 if __name__ == '__main__':
